8b0614c60a3a621e5aa311a71988bcf4d59a1068
petercai0131 <caiyongmao0420@gmail.com>
2025-10-16 18:06:55 -0700

    ex04

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,11 +1,18 @@
 // add a button titled click me
 let count = 0;
+let colors = ["Orchid","Coral","HotPink","Plum"];
 // when it is clicked
 $("#needy-button").click(function(){
     // show how many times
     // add one to the total number
-    count = count + 1;
     // show the total number
-    $("#needy-button").html("You clicked me " + count +" times!");
+    $("#needy-button").html("Clicks: " + count +" Color: " + colors[count]);
+    $("body").css({
+        "background-color": colors[count],
+    });
+    count = count + 1;
+    // if (count > 3){
+    //     count = 0;
+    // };
 });
 // a top limit
